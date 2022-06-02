62d02ecc3f1003a50baf29b84e334886e2df0264
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-06-02 14:00:56 -0500

    added icons

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ var currentTime = document.querySelector("#currentDay");
 
 var time =function() {
     var today = $(currentTime).find()
-    var date = moment(today).format("dddd , MMM Do YYYY")
+    var date = moment(today).format("dddd , MMM Do YY")
     currentTime.textContent= date;
 }
-time()
+time();
