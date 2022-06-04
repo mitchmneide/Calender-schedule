938a120c6ff4a2f6ac2bf56ca0f306d0304ab174
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-06-04 10:19:31 -0500

    added color fun

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,33 @@
-var currentTime = document.querySelector("#currentDay");
-
-
+var todayDay = document.querySelector("#currentDay");
+var timeBlock = document.getElementsByClassName("time-block");
+var saveBtn =document.getElementsByClassName(".saveBtn")
+let currentHour = moment().format("h:mm:ss a");
+// the current time/day funtion
 var time =function() {
-    var today = $(currentTime).find()
-    var date = moment(today).format("dddd , MMM Do YY")
-    currentTime.textContent= date;
+    var today = $(todayDay).find()
+    var date = moment(today).format("dddd , MMM Do YYYY")
+    todayDay.textContent= date;
 }
+
+var colorcode = function() {
+  $(".time-block").each( function(){
+      var timeTest = parseInt($(this).attr("id"));
+      hour =parseInt(currentHour);
+      console.log(timeTest);
+      console.log(hour);
+      
+      if (hour < timeTest){
+          $(this).addClass('future');
+      }
+      else if (hour === timeTest){
+          $(this).addClass("present");
+      }
+      else {
+          $(this).addClass("past")
+      }
+  })
+}
+
+
 time();
+colorcode();
--- script.js
+++
@@ -1,8 +0,0 @@
-var currentTime = document.querySelector("#currentDay");
-
-var time =function() {
-    var today = new Date();
-    var date = today.getFullYear()+ '-'+(today.getMonth()+1)+'-'+today.getDate();
-    currentTime.textContent = date;
-}
-time()
